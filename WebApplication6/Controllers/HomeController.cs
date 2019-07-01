17b84f049297d63e19381e311a1544a3256666e6
AliaaYasser <48435964+AliaaYasser@users.noreply.github.com>
2019-07-01 12:19:49 +0200

    first commit

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExcelDataReader;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+                    // to get started. This is how we avoid dependencies on ACE or Interop:
+                    Stream stream = upload.InputStream;
+
+                    // We return the interface, so that
+                    IExcelDataReader reader = null;
+
+
+                    if (upload.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else if (upload.FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+
+                    reader.AsDataSet();
+
+                    DataSet result = reader.AsDataSet();
+                    reader.Close();
+                    
+
+                    return View(result.Tables[0]);
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
+            return View();
+        }
+
+
+
+
+        public List<IRow> GetExcellSheetRows(string filePath, long parsedFileId, bool skipFirstRow = true)
+        {
+            List<IRow> ExcellSheetRowList = new List<IRow>();
+            try
+            {
+                FileStream FS = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                List<ISheet> Sheetlist = new List<ISheet>();
+                int totalRowCount = new int();
+                #region Check Type Of Excel
+                if (filePath.EndsWith(".xls"))
+                {
+                    #region old excel sheet
+                    HSSFWorkbook Workbook = new HSSFWorkbook(FS);
+                    for (int i = 0; i < Workbook.Count; i++)
+                    {
+                        ISheet sheet = Workbook.GetSheetAt(i);
+                        Sheetlist.Add(sheet);
+                        totalRowCount += sheet.PhysicalNumberOfRows;
+                        if (skipFirstRow && sheet.PhysicalNumberOfRows > 1)
+                            totalRowCount--;
+                    }
+                    for (int j = 0; j < Sheetlist.Count; j++)
+                    {
+                        if (Sheetlist[j].IsActive)
+                        {
+                            System.Collections.IEnumerator rows = Sheetlist[j].GetRowEnumerator();
+                            if (skipFirstRow)
+                            {
+                                rows.MoveNext();
+                            }
+                            while (rows.MoveNext())
+                            {
+                                IRow row = (XSSFRow)rows.Current;
+                                ExcellSheetRowList.Add(row);
+                            }
+                        }
+                    }
+                    #endregion
+                }
+                else
+                {
+                    #region excel 2007 and later
+                    FS.Position = 0;
+                    XSSFWorkbook Workbook = new XSSFWorkbook(FS);
+                    for (int i = 0; i < Workbook.Count; i++)
+                    {
+                        Sheetlist.Add(Workbook.GetSheetAt(i));
+                    }
+                    for (int j = 0; j < Sheetlist.Count; j++)
+                    {
+                        if (Sheetlist[j].IsActive)
+                        {
+                            System.Collections.IEnumerator rows = Sheetlist[j].GetRowEnumerator();
+                            // skip first row if required... it may be the header 
+                            if (skipFirstRow)
+                            {
+                                rows.MoveNext();
+                            }
+                            while (rows.MoveNext())
+                            {
+                                IRow row = (XSSFRow)rows.Current;
+                                ExcellSheetRowList.Add(row);
+                            }
+                        }
+                    }
+                    #endregion
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                #region Log Exception
+                Log(LogEnum.LogWriteType.Both, LogEnum.LogMethodType.Method, MethodBase.GetCurrentMethod().Name, ex, LogEnum.View.Web);
+                #endregion
+            }
+            return ExcellSheetRowList;
+        }
+
+
+        public bool GenerateExcelSheetWithoutDownload(DataTable dataTable, string exportingSheetPath, out string exportingFileName)
+        {
+            #region Validate the parameters and Generate the excel sheet
+            bool returnValue = false;
+            exportingFileName = Guid.NewGuid().ToString() + ".xlsx";
+
+            try
+            {
+                string excelSheetPath = string.Empty;
+                #region Check If The directory is exist
+                if (!Directory.Exists(exportingSheetPath))
+                {
+                    Directory.CreateDirectory(exportingSheetPath);
+                }
+
+                excelSheetPath = exportingSheetPath + exportingFileName;
+                FileInfo fileInfo = new FileInfo(excelSheetPath);
+                #endregion
+
+                #region Write stream to the file
+                MemoryStream ms = DataToExcel(dataTable);
+                byte[] blob = ms.ToArray();
+                if (blob != null)
+                {
+                    using (MemoryStream inStream = new MemoryStream(blob))
+                    {
+                        FileStream fs = new FileStream(excelSheetPath, FileMode.Create);
+                        inStream.WriteTo(fs);
+                        fs.Close();
+                    }
+                }
+                ms.Close();
+                returnValue = true;
+                #endregion
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return returnValue;
+            #endregion
+        }
+    }
+}
---
+++ WebApplication6/Controllers/ValuesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExcelDataReader;
+namespace WebApplication6.Controllers
+
+{
+    public class ValuesController : ApiController
+    {
+
+        public void Upload()
+        {
+            return ;
+        }
+
+
+        [ValidateAntiForgeryToken]
+        public DataSet Upload(HttpPostedFileBase upload)
+        {
+
+          
+            if (ModelState.IsValid)
+            {
+
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+                    // to get started. This is how we avoid dependencies on ACE or Interop:
+                    Stream stream = upload.InputStream;
+
+                    // We return the interface, so that
+                    IExcelDataReader reader = null;
+
+
+                    if (upload.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                        
+                    }
+                    else if (upload.FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        string c= "This file format is not supported";
+                        return c;
+                    }
+
+                    reader.AsDataSet();
+
+                    DataSet result = reader.AsDataSet();
+                    reader.Close();
+
+                    return result;
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
+            return;
+        }
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
